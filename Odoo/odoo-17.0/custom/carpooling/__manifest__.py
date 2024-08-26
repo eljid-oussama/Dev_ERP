32ef915277f54a71b5c927dcbf96f06ace8e79c5
Oussama <eljidoussama155@gmail.com>
2024-08-26 17:33:22 +0200

    creation d'un nouveau groupe d'acces :Carpooling Administrator

--- Odoo/odoo-17.0/custom/carpooling/__manifest__.py
+++ Odoo/odoo-17.0/custom/carpooling/__manifest__.py
@@ -28,6 +28,7 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
+        'security/groups.xml',
         'views/views.xml',
         'views/templates.xml',
         'views/carpooling_views.xml',
