87dc2c13b3baea007a8f02604e2d2155f8776c5f
Oussama <eljidoussama155@gmail.com>
2024-09-02 17:17:55 +0200

    ajout d'un codebare automatiquement lors de la creation d'un nouveau produit

--- Odoo/odoo-17.0/custom/carpooling/models/__init__.py
+++ Odoo/odoo-17.0/custom/carpooling/models/__init__.py
@@ -6,4 +6,5 @@ from . import car_car
 from . import tag_tag
 from . import res_partner
 from . import sale_order_line
+from . import product_template
 
---
+++ Odoo/odoo-17.0/custom/carpooling/models/product_template.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+from random import randint
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    blacklist_countries_ids = fields.Many2many('res.country', string="Blacklist Countries")
+    blacklist_countries_ids = fields.Many2many('res.partner', string="Blacklist Contacts")
+
+#fonction appele a la creation d'un produit , il genere un numero random qui sera le code barre
+    def _create_new_barcode(self):
+        current_seq = str(randint(1, 9999999999999)).zfill(13)
+        #la on essaye d'ajouter des conditions sur le codebare (ici on l'oblge de commence par 4)
+        while current_seq[0] != '4' and self.env['product.template'].search([('barcode', '=' , current_seq)]):
+            current_seq = str(randint(1, 9999999999999)).zfill(13)
+        self.barcode = current_seq
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        recs = super().create(vals_list)
+        for rec in recs:
+            if not rec.barcode:
+                rec._create_new_barcode()
+        return recs
+
